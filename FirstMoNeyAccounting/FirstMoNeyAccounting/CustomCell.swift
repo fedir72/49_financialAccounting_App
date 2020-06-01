862e3c5f6822f8cc3a993adf3214efd9f82e5b6c
fedir72 <fedir72@ua.fm>
2020-06-01 17:28:55 +0300

    connected view to controller

---
+++ FirstMoNeyAccounting/FirstMoNeyAccounting/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  FirstMoNeyAccounting
+//
+//  Created by fedir on 01.06.2020.
+//  Copyright © 2020 fedir. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var recordImage: UIImageView!
+    
+    @IBOutlet weak var recordCategory: UILabel!
+    
+    @IBOutlet weak var recordCost: UILabel!
+    
+}
--- FirstMoNeyAccounting/FirstMoNeyAccounting/ViewController.swift
+++ FirstMoNeyAccounting/FirstMoNeyAccounting/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  FirstMoNeyAccounting
 //
@@ -10,14 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     
+    //переменная для контроля текста  0 или нет
+    var stillTyping = false
+    
+    @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet var numberFromKeyboard: [UIButton]!
+    
+    var categoryName = ""
+    var displayValue = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         numberFromKeyboard.forEach {$0.layer.cornerRadius = 10}
+        tableView.delegate = self
+        tableView.dataSource =  self
     }
-
+    
+     
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        let number = sender.currentTitle!
+        //проверка на 0 на дисплее
+        if stillTyping {
+            if displayLabel.text!.count < 12 {
+                displayLabel.text! += number
+            }
+            
+        }else{
+            displayLabel.text = number
+            stillTyping = true
+        }
+    }
+    
+    
+    //удаление всей строки на дисплее
+    @IBAction func resetButton(_ sender: UIButton) {
+        displayLabel.text = "0"
+        stillTyping = false
+    }
+  
+    
+    //удаление последнего символа
+    @IBAction func removeLastButton(_ sender: Any) {
+        if displayLabel.text != "0" {
+            let txt = displayLabel.text!.dropLast()
+            displayLabel.text = String(txt)
+        }
+    }
+    
+    @IBAction func categoryPressed(_ sender: UIButton) {
+         
+        categoryName = sender.currentTitle!
+        displayValue = displayLabel.text!
+        displayLabel.text = "0"
+        stillTyping = false
+        
+        print(categoryName  , displayValue)
+    }
+    
+    
+    
 
 }
 
+ extension UIViewController: UITableViewDelegate,UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 62
+        
+    }
+    
+    
+ }
